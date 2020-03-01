4b0ead23976c8def29cc32b56f0f55bdf3a05e42
Shabbir Bharmal <shabbir.bharmal@arsenaltech.com>
2020-03-01 13:36:52 +0530

    - Created Feature Request Form, added validation, stored inputs.

--- feature-request.php
+++ feature-request.php
@@ -1,3 +1,36 @@
 <?php
 include_once 'config.php';
 
+// Include header
+$page = 'feature-request';
+$page_title = 'Feature Request';
+include_once F_ROOT.'parts/layout/head.php';
+
+$msg = '';
+if($_SESSION['feature-request-error']){
+
+}
+?>
+	<!--Body content-->
+
+	<!-- Auth navigation -->
+	<header>
+		<?php include_once(F_ROOT.'parts/header-auth.php'); ?>
+	</header>
+
+	<div class="container-fluid mt-3">
+
+		<div class="row mb-3">
+			<div class="col-12">
+				<h2 class="m-0"><img src="<?php echo W_ROOT; ?>/favicon.ico" style="height:30px;margin-right:10px">Feature Request</h2>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-12"><?php include_once(F_ROOT.'parts/feature-request-form.php'); ?></div>
+		</div>
+	</div>
+
+<?php
+// Include footer
+include_once F_ROOT.'parts/layout/footer.php';
---
+++ js/feature-request.js
@@ -0,0 +1,28 @@
+$(function () {
+  function formValidation() {
+    $("#feature_request_form").validate({
+      rules   : {
+        f_title: {
+          required : true,
+          maxlength: 55
+
+        }
+      },
+      // Setting error messages for the fields
+      messages: {
+        f_title: {
+          required : "Please enter feature title.",
+          maxlength: "Feature title must be less than 55 characters."
+        }
+      }
+    });
+  }
+
+  $('#f_due_date').datetimepicker({
+    format: 'YYYY-MM-DD'
+  });
+
+  formValidation();
+});
+
+
