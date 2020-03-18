285299fa82abce78a9324ba1f0601d6ae867c2a5
shabbir.bharmal <shabbir.bharmal@arsenaltech.com>
2020-03-18 09:25:03 +0530

    - I have implemented as per provided instruction in work_5.docx file   5) Edit Feature and Epics of my Topics [150]   Note:   I have implemented Edit Feature by Topics functionality. Edit epic by topic functionality is pending due to one query.

---
+++ parts/manage-epic-request/features_tab.php
@@ -0,0 +1,6 @@
+<br />
+<div class="form-row">
+    <div class="form-group col-12">
+        sorry, not yet ready ;-)
+    </div>
+</div>
---
+++ parts/manage-feature-request/features_tab.php
@@ -0,0 +1,5 @@
+</br>
+<div class="form-group row">
+    <label class="col-2 col-xs-12 col-form-label">Comming Soon
+    </label>
+</div>
